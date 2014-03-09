8ff00fff23bd1317746a360610cd3ab062b5d7c6
Jason Siefken <siefkenj@gmail.com>
2014-03-08 20:33:03 -0800

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ web-hanabi
 ==========
 
 Web based version of the card game Hanabi
+
+Development
+-----------
+
+To run `web-hanabi` you need have `Node.js` installed.
+Once installed, go to the `web-hanabi` directory and type
+
+	node server.js
+
+on the command line to start the `web-hanabi` server.
+If all goes well, a message will be printed stating that
+a server was successfully started on port 3000.  You can then
+access `localhost:3000` from your web browser.
+
+Files listed under the `public/` directory are served
+statically.
+
+	* `websockets_test.html`:
+	 Navigate to `localhost:3000/websockets_test.html`
+	 to test the websockets code.  If you navigate
+	 to that page from several different browser windows
+	 and click the 'Send Random Message' button,
+	 a message should be sent between all instances of
+	 browsers at that page!
+
+	 If you want to see syncronization between different
+	 computers, navigate a browser on another computer
+	 to `<your IP>:3000/websockets_test.html`
---
+++ server.js
@@ -0,0 +1,47 @@
+/*
+ * Basic Node.js express server that runs a socket.io instance
+ * to mirror all data sent by one client to all others (in the same
+ * socket.io room)
+ */
+
+var PORT = 3000;
+
+// Preliminaries
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+// Statically server pages from the public directory
+app.configure( function() {
+    app.use(express.static(__dirname + '/public'));
+});
+
+// Start the server
+server.listen(PORT);
+console.log('Server listening on port ' + PORT);
+
+
+
+// Handle all the websocket connections
+io.sockets.on('connection', function(socket) {
+    socket.on('disconnect', function () { console.log('Disconnecting Client'); });
+
+    // rooms allow us to limit our broadcasts to others in the same room.
+    socket.on('room', function(room) {
+        console.log('Joining room', room);
+        socket.join(room)
+        socket.currentRoom = room; 
+    });
+
+    // when new data is broadcast by a client, emit it to all
+    // other clients in the same room
+    socket.on('broadcast', function (data) {
+        console.log('Got message to retransmit', data, socket.currentRoom);
+        var room = socket.currentRoom;
+        // everyone in the same room as the broadcaster will get the data
+        // relayed to them
+        socket.in(room).broadcast.emit('message', {message: data});
+    });
+});
