ef7710cb8b71a4d76b368db65d23419c7fcbb99d
Jason Siefken <siefkenj@gmail.com>
2014-09-06 11:05:43 -0700

    Improved rendering of bokeh

--- public/js/bokeh-webworker.js
+++ public/js/bokeh-webworker.js
@@ -8,7 +8,7 @@ self.addEventListener('message', doBubbleBlend);
 
 function doBubbleBlend(e) {
     "use strict";
-    var i = 0, j = 0, k = 0, x = 0, y = 0, xpos = 0, ypos = 0, stride = 0, strideBubble = 0, alpha = 0.0, a = 0, b = 0;
+    var i = 0, j = 0, k = 0, x = 0, y = 0, xpos = 0, ypos = 0, stride = 0, strideBubble = 0, alpha = 0.0, alphaScaled = 0.0, a = 0, b = 0;
     var data = e.data;
 
     var numBubbles = data.numBubbles;
@@ -41,22 +41,47 @@ function doBubbleBlend(e) {
                 if (alpha === 0) {
                     continue;
                 }
+                alphaScaled = -alpha*(alpha-2);
 
-                // do screen blending of each channel
+                // do screen blend the bubbles onto the target buffer
                 a = targetBuffer.data[stride + 0]/255;
-                b = (sourceBuffer.data[stride + 0] + bubble.data[strideBubble + 0])/255;
+                b = alphaScaled*bubble.data[strideBubble + 0]/255;
                 targetBuffer.data[stride + 0] = 255*(1-(1-a)*(1-b));
                 a = targetBuffer.data[stride + 1]/255;
-                b = (sourceBuffer.data[stride + 1] + bubble.data[strideBubble + 1])/255;
+                b = alphaScaled*bubble.data[strideBubble + 1]/255;
                 targetBuffer.data[stride + 1] = 255*(1-(1-a)*(1-b));
                 a = targetBuffer.data[stride + 2]/255;
-                b = (sourceBuffer.data[stride + 2] + bubble.data[strideBubble + 2])/255;
+                b = alphaScaled*bubble.data[strideBubble + 2]/255;
                 targetBuffer.data[stride + 2] = 255*(1-(1-a)*(1-b));
                 // copy the alpha channel directly
                 a = targetBuffer.data[stride + 3]/255;
-                b = bubble.data[strideBubble + 3]/255
-                targetBuffer.data[stride + 3] += 255*(a + b - a*b);
+                b = bubble.data[strideBubble + 3]/255;
+                targetBuffer.data[stride + 3] = 255*(a + b - a*b);
+                
             }
+        }
+    }
+    for (j = 0; j < targetBuffer.height; j++) {
+        for (i = 0; i < targetBuffer.width; i++) {
+            stride = j*targetBuffer.width*4 + i*4;
+            
+            alpha = targetBuffer.data[stride + 3]/255;
+            // if there is nothing to blend, do nothing
+            if (alpha === 0) {
+                continue;
+            }
+            alphaScaled = -alpha*(alpha-2);
+
+            // do screen blending with the sourceBuffer but leave the alpha channel alone
+            a = targetBuffer.data[stride + 0]/255 + alphaScaled/4;
+            b = sourceBuffer.data[stride + 0]/255;
+            targetBuffer.data[stride + 0] = 255*(1-(1-a)*(1-b));
+            a = targetBuffer.data[stride + 1]/255 + alphaScaled/4;
+            b = sourceBuffer.data[stride + 1]/255;
+            targetBuffer.data[stride + 1] = 255*(1-(1-a)*(1-b));
+            a = targetBuffer.data[stride + 2]/255 + alphaScaled/4;
+            b = sourceBuffer.data[stride + 2]/255;
+            targetBuffer.data[stride + 2] = 255*(1-(1-a)*(1-b));
         }
     }
 
